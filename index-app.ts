2168e4f1e70b070bf374773277388ae5c24e01bf
Meris Beganovic <42630924+mbeganovic3@users.noreply.github.com>
2019-07-11 17:30:40 -0400

    Add files via upload

---
+++ index-app.ts
@@ -0,0 +1,8 @@
+import { print, image } from "introcs";
+
+export let main = async () => {
+    print("Dear Mom,");
+    print("I love you and I am doing great in my first COMP class.");
+    image("https://media.giphy.com/media/3o7TKoWXm3okO1kgHC/giphy.gif");
+ };
+main();
